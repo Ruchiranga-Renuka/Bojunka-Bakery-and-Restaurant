47921fd49ac23f570e9bfbe5a1daee0b56fc26cd
Ruchiranga Renuka <mr.ruchirangarenuka@gmail.com>
2026-05-21 14:18:30 +0530

    Update OrderRepository.java

--- backend/src/main/java/com/bojunka/backend/repository/OrderRepository.java
+++ backend/src/main/java/com/bojunka/backend/repository/OrderRepository.java
@@ -3,5 +3,9 @@ package com.bojunka.backend.repository;
 import com.bojunka.backend.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, String> {
+    List<Order> findByCustomerInOrderByDateDesc(Collection<String> customers);
 }
